da24ab9c08799728f211d8b843089d96321fea55
AN-2000 <anirudh.inc.my.company@gmail.com>
2020-08-27 00:01:18 +0530

    Updates

---
+++ precache-manifest.1f04ee0b87aa9cb6d909b1bd79b73eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358550bd642050fe9ee94357d4ae4478",
+    "url": "/wikiBar/index.html"
+  },
+  {
+    "revision": "dee5583001cfe9fb2b88",
+    "url": "/wikiBar/static/js/2.3edd7e0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wikiBar/static/js/2.3edd7e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523ebeb67698119b4def",
+    "url": "/wikiBar/static/js/main.db7b1671.chunk.js"
+  },
+  {
+    "revision": "a10b237113f6279dea2f",
+    "url": "/wikiBar/static/js/runtime-main.890250cc.js"
+  }
+]);
